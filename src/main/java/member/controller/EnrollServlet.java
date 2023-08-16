d72a100b6c7c67944740cb751e8b14d8b10d9196
JS Kim <kiqbbq0602@gmail.com>
2023-08-16 12:51:28 +0900

    변경점

--- src/main/java/member/controller/EnrollServlet.java
+++ src/main/java/member/controller/EnrollServlet.java
@@ -2,6 +2,7 @@ package member.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -49,6 +50,23 @@ public class EnrollServlet extends HttpServlet {
 		// 3. 모델의 서비스 메소드로 값 전달 실행하고 결과 받기
 		int result = new MemberService().insertMember(member);
 		// 4. 받은 결과로 성공 또는 실패 페이지 내보내기
+		if(result > 0){
+			//회원 가입이 성공 했을때
+			response.sendRedirect("/first/views/member/loginPage.html");
+		}else{
+			// 회원 가입이 실패 했을때 error.jsp 페이지 내보내기 에러메세지를 보냄
+			RequestDispatcher view = request.getRequestDispatcher("views/common/error.jsp");
+			// 상대 경로만 사용 가능
+			
+			//지정한 뷰로 내보낼 정보나 객체가 있으면 request 에 기록 저장함
+			// request.setAttribute(String name, Object value);
+			request.setAttribute("message", "회원 가입 fail");
+
+			// view.forward()
+			// 뷰를 포워딩함
+			view.forward(request, response);
+
+		}
 	}
 
 	/**
